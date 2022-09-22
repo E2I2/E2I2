76b17295aed809f60a8b377f37895ad6df132858
okdaae <dhrekdo5@kakao.com>
2022-09-22 19:01:31 +0900

    09:22 커밋

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-var express = require("express");
-var controller = require("../controller/Cmain");
-var controllerSign = require("../controller/Csign");
-var controllerProfile = require("../controller/Cprofile");
-var testcontroller = require("../controller/Ctest");
-const router = express.Router();
+var express = require('express')
+var controller = require('../controller/Cmain')
+var controllerSign = require('../controller/Csign')
+var controllerProfile = require('../controller/Cprofile')
+// var testcontroller = require("../controller/Ctest");
+const router = express.Router()
 
-router.get("/", controller.main);
+router.get('/', controller.main)
 
-router.get("/signup", controllerSign.signup);
-router.post("/signup/post", controllerSign.signup_post);
-router.get("/signin", controllerSign.signin);
-router.get("/find", controllerSign.find);
+router.get('/signup', controllerSign.signup)
+router.post('/signup/post', controllerSign.signup_post)
+router.get('/signin', controllerSign.signin)
+router.get('/find', controllerSign.find)
 
-router.get("/profile", controllerProfile.profile);
-router.get("/matching", controllerProfile.matching);
+router.get('/profile', controllerProfile.profile)
+router.get('/matching', controllerProfile.matching)
 
-module.exports = router;
+module.exports = router
