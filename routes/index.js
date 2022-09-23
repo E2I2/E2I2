b47f41bb64c60438bbc9284ff3523573ca1852d0
hanchorong <hancrong@gmail.com>
2022-09-23 13:46:21 +0900

    login_axios

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get("/", controller.main);
 router.get("/signup", controllerSign.signup);
 router.post("/signup/post", controllerSign.signup_post);
 router.get("/signin", controllerSign.signin);
+router.post("/signin/post", controllerSign.signin_post);
 router.get("/find", controllerSign.find);
 
 router.get("/profile", controllerProfile.profile);
