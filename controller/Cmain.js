a61ae4f7789ed4658acfeec33852b78c1baea103
Deng <edaero22@naver.com>
2022-09-23 18:28:16 +0900

    add login session

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -8,3 +8,23 @@ exports.main = (req, res) => {
     res.render("index", { isLogin: false });
   }
 };
+
+exports.logout = (req, res) => {
+  const user = req.session.user;
+  if (user != undefined) {
+    req.session.destroy((err) => {
+      res.send(
+        `<script>
+          location.href="/";
+        </script>`
+      );
+    });
+  } else {
+    res.send(
+      `<script>
+      aleart("잘못된 접근입니다.");
+      location.href="/";
+      </script>`
+    );
+  }
+};
