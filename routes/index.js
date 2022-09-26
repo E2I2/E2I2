10f51a5c22f3ea43c76d9ba94f35e7543c2c6306
CHOIALTH <dnjswns4545@gmail.com>
2022-09-26 15:44:28 +0900

    multer adding

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 router.get("/", controller.main);
 
-<<<<<<< HEAD
 router.get('/signup', controllerSign.signup)
 router.post('/signup/post', controllerSign.signup_post)
 router.get('/signin', controllerSign.signin)
@@ -19,16 +18,6 @@ router.get('/profile', controllerSign.profile)
 router.post('/profile/uploadImg', controllerSign.profile_upload)
 router.get('/matching', controllerSign.matching)
 router.get('/chatting', controllerChat.chat_main)
-=======
-router.get("/signup", controllerSign.signup);
-router.post("/signup/post", controllerSign.signup_post);
-router.get("/signin", controllerSign.signin);
-router.post("/signin/post", controllerSign.signin_post);
-router.get("/logout", controller.logout);
-router.get("/find", controllerSign.find);
-router.get("/profile", controllerSign.profile);
-router.post("/profile/uploadImg", controllerSign.profile_upload);
-router.get("/matching", controllerSign.matching);
->>>>>>> upstream/feature
+
 
 module.exports = router;
