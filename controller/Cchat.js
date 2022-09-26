10ca89b1b32fa9b49d373d54b19a87e15dc96ee1
CHOIALTH <dnjswns4545@gmail.com>
2022-09-27 00:51:17 +0900

    chat : socket1

--- controller/Cchat.js
+++ controller/Cchat.js
@@ -1,4 +1,14 @@
+const session = require("express-session");
+const { Userinfo, sequelize } = require("../model/main");
+
 exports.chat_main = (req, res) => {
-    res.render("chatting");
-  };
-  
+  Userinfo.findOne({
+    where : {
+      id: req.session.user[1]
+    },
+  }).then((result)=>{
+      res.render("chatting", { 
+      sendNick : result.nick
+      });
+    })
+};
