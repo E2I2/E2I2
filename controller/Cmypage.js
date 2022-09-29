0a0a24d0941dac2395642a3b3da1a5091ad53e43
okdaae <dhrekdo5@kakao.com>
2022-09-29 17:33:00 +0900

    fix conflct

--- controller/Cmypage.js
+++ controller/Cmypage.js
@@ -1,42 +1,10 @@
 const session = require('express-session')
 const { Userinfo, Sequelize } = require('../model/main')
-
 exports.mypage = (req, res) => {
   res.render('myPage')
 }
-
 exports.editProfile = (req, res) => {
-<<<<<<< HEAD
-  res.render('editProfile')
-}
-
-exports.myPage = (req, res) => {
-  res.render('myPage')
-}
-
-exports.editProfile_upload = (req, res) => {
-  console.log('edit_profileInput', req.body)
-  console.log('edit_Filename:', req.file.filename)
-
-  Userinfo.update(
-    {
-      imgurl: './uploads/' + req.file.filename,
-      job: req.body.job,
-      interest: req.body.interest,
-      specialty: req.body.specialty,
-      userdesc: req.body.userdesc,
-    },
-    {
-      where: {
-        id: req.session.user[1],
-      },
-    }
-  ).then((result) => {
-    res.send('프로필수정완료')
-  })
-}
-=======
-  const user = req.session.user;
+  const user = req.session.user
   if (user != undefined) {
     Userinfo.findOne({
       where: {
@@ -44,35 +12,34 @@ exports.editProfile_upload = (req, res) => {
         name: req.session.user[0],
       },
     }).then((result) => {
-      var img = result.imgurl;
-      var job = result.job;
-      var interest = result.interest;
-      var specialty = result.specialty;
-      var desc = result.userdesc;
-      res.render("editProfile", {
+      var img = result.imgurl
+      var job = result.job
+      var interest = result.interest
+      var specialty = result.specialty
+      var desc = result.userdesc
+      res.render('editProfile', {
         img: img,
         job: job,
         interest: interest,
         specialty: specialty,
         desc: desc,
-      });
-    });
+      })
+    })
   } else {
     res.send(
       `<script>
         alert("잘못된 접근입니다. 로그인 후 이용해주세요.");
         location.href="/signin";
         </script>`
-    );
+    )
   }
-};
-
+}
 exports.editProfile_upload = (req, res) => {
-  console.log("edit_profileInput", req.body.editFileAxios);
-  if (req.body.editFileAxios != "undefined") {
+  console.log('edit_profileInput', req.body.editFileAxios)
+  if (req.body.editFileAxios != 'undefined') {
     Userinfo.update(
       {
-        imgurl: "/uploads/" + req.file.filename,
+        imgurl: '/uploads/' + req.file.filename,
         job: req.body.job,
         interest: req.body.interest,
         specialty: req.body.specialty,
@@ -84,8 +51,8 @@ exports.editProfile_upload = (req, res) => {
         },
       }
     ).then((result) => {
-      res.send("프로필수정완료");
-    });
+      res.send('프로필수정완료')
+    })
   } else {
     Userinfo.update(
       {
@@ -100,8 +67,7 @@ exports.editProfile_upload = (req, res) => {
         },
       }
     ).then((result) => {
-      res.send("프로필수정완료");
-    });
+      res.send('프로필수정완료')
+    })
   }
-};
->>>>>>> upstream/feature
+}
