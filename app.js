1153d57f3978cdd97a634b24c25bb595ed323a8d
ee.jy <99079176+Edaero@users.noreply.github.com>
2022-09-29 18:38:20 +0900

    Revert "Intermediate Merge"

--- app.js
+++ app.js
@@ -1,49 +1,15 @@
 const express = require("express");
-const axios = require("axios");
-const session = require("express-session");
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-
 const port = 8000;
 
 app.set("view engine", "ejs");
 app.use("/static", express.static("static"));
-app.use("/uploads", express.static("uploads"));
-app.use("/axios", express.static("node_modules/axios"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// 세션
-app.use(
-  session({
-    secret: "1234",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    },
-  })
-);
 
 const router = require("./routes");
-
 app.use("/", router);
 
-io.on("connection", function (socket) {
-  // 최초 입장했을 때
-  console.log("Server Socket Connected", socket.id);
-
-  socket.on("entry", (data) => {});
-
-  socket.on("send", (data) => {
-    const sendData = {
-      sendNick: data.sendNick,
-      msg: data.msg,
-    };
-    socket.emit("newMessage", sendData);
-  });
-});
-
-http.listen(port, () => {
+app.listen(port, () => {
   console.log("server open: ", port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,14 @@
-var express = require('express')
-var controller = require('../controller/Cmain')
-var controllerSign = require('../controller/Csign')
-var controllerChat = require('../controller/Cchat')
-var controllerMy = require('../controller/Cmypage')
-// var controllerProfile = require("../controller/Cprofile");
+var express = require("express");
+var controller = require("../controller/Cmain");
+var controllerSign = require("../controller/Csign");
+var controllerProfile = require("../controller/Cprofile");
+const router = express.Router();
 
-const multer = require('multer')
-const path = require('path')
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, done) => {
-      done(null, 'uploads')
-    },
-    filename: (req, file, done) => {
-      const ext = path.extname(file.originalname)
-      const filename = path.basename(file.originalname, ext) + ext
-      // + '_' + Date.now() + ext
-      done(null, filename)
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-})
+router.get("/", controller.main);
 
-const router = express.Router()
+router.get("/signup", controllerSign.signup);
+router.get("/signin", controllerSign.signin);
 
-router.get('/', controller.main)
+router.get("/profile", controllerProfile.profile);
 
-router.get('/signup', controllerSign.signup)
-router.post('/signup/post', controllerSign.signup_post)
-router.get('/signin', controllerSign.signin)
-router.post('/signin/post', controllerSign.signin_post)
-router.get('/logout', controller.logout)
-router.get('/find', controllerSign.find)
-router.post('/find/id', controllerSign.find_id)
-router.post('/find/pw', controllerSign.find_pw)
-
-router.get('/profile', controllerSign.profile)
-router.post(
-  '/profile/uploadImg',
-  upload.single('userFileAxios'),
-  controllerSign.profile_upload
-)
-router.get('/matching', controllerSign.matching)
-router.get('/chatting', controllerChat.chat_main)
-
-router.get('/myPage', controllerMy.mypage)
-router.get('/editProfile', controllerMy.editProfile)
-router.post(
-  '/editProfile/editUpload',
-  upload.single('editFileAxios'),
-  controllerMy.editProfile_upload
-)
-
-module.exports = router
+module.exports = router;
