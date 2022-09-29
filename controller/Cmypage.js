117b0fc2f92cd911bdc7c051819a7e6efaf11d03
okdaae <dhrekdo5@kakao.com>
2022-09-29 17:02:29 +0900

    revise viewfile

--- controller/Cmypage.js
+++ controller/Cmypage.js
@@ -1,21 +1,25 @@
-const session = require("express-session");
-const { Userinfo, Sequelize } = require("../model/main");
+const session = require('express-session')
+const { Userinfo, Sequelize } = require('../model/main')
 
 exports.mypage = (req, res) => {
-  res.render("myPage");
-};
+  res.render('myPage')
+}
 
 exports.editProfile = (req, res) => {
-  res.render("editProfile");
-};
+  res.render('editProfile')
+}
+
+exports.myPage = (req, res) => {
+  res.render('myPage')
+}
 
 exports.editProfile_upload = (req, res) => {
-  console.log("edit_profileInput", req.body);
-  console.log("edit_Filename:", req.file.filename);
+  console.log('edit_profileInput', req.body)
+  console.log('edit_Filename:', req.file.filename)
 
   Userinfo.update(
     {
-      imgurl: "./uploads/" + req.file.filename,
+      imgurl: './uploads/' + req.file.filename,
       job: req.body.job,
       interest: req.body.interest,
       specialty: req.body.specialty,
@@ -27,6 +31,6 @@ exports.editProfile_upload = (req, res) => {
       },
     }
   ).then((result) => {
-    res.send("프로필수정완료");
-  });
-};
+    res.send('프로필수정완료')
+  })
+}
